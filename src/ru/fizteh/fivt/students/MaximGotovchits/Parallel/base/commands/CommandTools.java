3985e62054ca44c2a69eea09283a615a912aaeaa
MaximGotovchits <Gotovchits.m@mail.ru>
2015-03-22 20:14:21 +0300

    Parallel
    ver 100500.1

--- src/ru/fizteh/fivt/students/MaximGotovchits/Parallel/base/commands/CommandTools.java
+++ src/ru/fizteh/fivt/students/MaximGotovchits/Parallel/base/commands/CommandTools.java
@@ -1,17 +1,9 @@
 package ru.fizteh.fivt.students.MaximGotovchits.Parallel.base.commands;
 
-import ru.fizteh.fivt.students.MaximGotovchits.Parallel.objects.ObjectStoreable;
 import ru.fizteh.fivt.students.MaximGotovchits.Parallel.objects.ObjectTableProvider;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 
 public class CommandTools {
     public static final String DATA_BASE_NAME = System.getProperty("fizteh.db.dir");
